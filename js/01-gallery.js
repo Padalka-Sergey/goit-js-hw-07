3c0716c6692ded361953da8f81aca22badc2198b
Padalka Sergey <serega443@gmail.com>
2022-10-09 22:32:21 +0300

    js-08.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const itemsContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+itemsContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+itemsContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      console.log(evt.code);
+      instance.close();
+    }
+  });
+});
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const itemsContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+itemsContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+itemsContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+});
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      
+        <li>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a></li>
+        `;
+    })
+    .join("");
+}
